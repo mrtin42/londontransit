9520629979927279f27586e5a517cf25cf20cece
martin <martini@tubee.dev>
2023-08-25 03:01:42 +0100

    layout updates

--- docs/assets/scripts/navbar.js
+++ docs/assets/scripts/navbar.js
@@ -1,15 +1,11 @@
-// trigger function on scroll
+
 window.onscroll = function() {scrollFunction()};
 
-// function to change navbar on scroll
 function scrollFunction() {
-    //check if scroll is greater than 300px
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        // make navbar background solid #333
-        document.getElementById("topnav").style.backgroundColor = "#333";
+    if (document.body.scrollTop > 320 || document.documentElement.scrollTop > 320) {
+        document.getElementById("topnav").style.backgroundColor = "#0a1047";
 
     } else {
-        // return to transparent background
         document.getElementById("topnav").style.backgroundColor = "transparent";
     }
 }
